dac060a91cd4924471fdd5238c8d871ae892bc2a
Yichen Yao <yyao0060@student.monash.edu>
2024-10-17 16:33:48 +1100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
